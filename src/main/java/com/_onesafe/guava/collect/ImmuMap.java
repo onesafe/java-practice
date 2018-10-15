28b96b437d39b52593af39ae37f9b12185d46e25
Wang Yiping <wangyiping@4paradigm.com>
2018-10-15 12:50:02 +0800

    feat: guava collect ImmutableMap

---
+++ src/main/java/com/_onesafe/guava/collect/ImmuMap.java
@@ -0,0 +1,14 @@
+package com._onesafe.guava.collect;
+
+import com.google.common.collect.ImmutableMap;
+
+/**
+ * Created by onesafe on 15/10/2018.
+ */
+public class ImmuMap {
+
+    public static final ImmutableMap<String, String> MY_MAP = ImmutableMap.of(
+            "ww", "ok",
+            "yy", "bye"
+    );
+}
