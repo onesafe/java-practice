6dbd70179eef4f4561f4b9bfbe563f925e662bfd
Wang Yiping <wangyiping@4paradigm.com>
2018-10-23 17:30:17 +0800

    feat: Fight

---
+++ src/main/java/com/_onesafe/thread/Fight/FightQuery.java
@@ -0,0 +1,10 @@
+package com._onesafe.thread.Fight;
+
+import java.util.List;
+
+/**
+ * Created by onesafe on 23/10/2018.
+ */
+public interface FightQuery {
+    List<String> get();
+}
---
+++ src/main/java/com/_onesafe/thread/Fight/FightQueryExample.java
@@ -0,0 +1,47 @@
+package com._onesafe.thread.Fight;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Created by onesafe on 23/10/2018.
+ */
+public class FightQueryExample {
+
+    private static List<String> fightCompany = Arrays.asList("CSA", "CEA", "HNA");
+
+    public static void main(String[] args) {
+        List<String> results = search("SH", "BJ");
+        System.out.println("===============result===============");
+        results.forEach(System.out::println);
+    }
+
+    private static List<String> search(String original, String dest) {
+        final List<String> result = new ArrayList<>();
+
+        List<FightQueryTask> tasks = fightCompany.stream().map(f -> createSearchTask(f, original, dest))
+                .collect(toList());
+
+        tasks.forEach(Thread::start);
+
+        tasks.forEach(t ->
+        {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        tasks.stream().map(FightQuery::get).forEach(result::addAll);
+
+        return result;
+    }
+
+    private static FightQueryTask createSearchTask(String fight, String original, String dest) {
+        return new FightQueryTask(fight, original, dest);
+    }
+}
