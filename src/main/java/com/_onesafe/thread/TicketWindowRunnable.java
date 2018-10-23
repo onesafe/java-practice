aca945f21248a24b40080bf7b1f4b9439257b46e
Wang Yiping <wangyiping@4paradigm.com>
2018-10-23 15:14:14 +0800

    feat: thread runnable

---
+++ src/main/java/com/_onesafe/thread/TicketWindowRunnable.java
@@ -0,0 +1,39 @@
+package com._onesafe.thread;
+
+/**
+ * Created by onesafe on 23/10/2018.
+ */
+public class TicketWindowRunnable implements Runnable {
+
+    private int index = 1;
+
+    private final static int MAX = 50;
+
+    @Override
+    public void run() {
+
+        while(index <= MAX) {
+            System.out.println(Thread.currentThread() + " 的号码是： " + (index++));
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        final TicketWindowRunnable task = new TicketWindowRunnable();
+
+        Thread windowThread1 = new Thread(task, "一号窗口");
+        Thread windowThread2 = new Thread(task, "二号窗口");
+        Thread windowThread3 = new Thread(task, "三号窗口");
+        Thread windowThread4 = new Thread(task, "四号窗口");
+
+        windowThread1.start();
+        windowThread2.start();
+        windowThread3.start();
+        windowThread4.start();
+    }
+}
