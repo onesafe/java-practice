19c3c9922f9cd81fa7bc017e769531a99c940f77
Wang Yiping <wangyiping@4paradigm.com>
2018-10-25 14:00:51 +0800

    feat: thread hook

---
+++ src/main/java/com/_onesafe/thread/ThreadHook.java
@@ -0,0 +1,39 @@
+package com._onesafe.thread;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by onesafe on 25/10/2018.
+ */
+public class ThreadHook {
+
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("The hook thread 1 is running.");
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("The hook thread 1 will exit.");
+            }
+        });
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("The hook thread 2 is running.");
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("The hook thread 2 will exit.");
+            }
+        });
+
+        System.out.println("The program is stopping..");
+    }
+}
