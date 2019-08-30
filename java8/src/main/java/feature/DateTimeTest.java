e948a9da920e71ed6f9e9e1ab6733ddf7f6697f1
Wang Yiping <wangyiping@4paradigm.com>
2019-08-30 15:42:08 +0800

    java8 date time

---
+++ java8/src/main/java/feature/DateTimeTest.java
@@ -0,0 +1,116 @@
+package feature;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by onesafe on 30/08/2019 3:35 PM.
+ */
+public class DateTimeTest {
+
+    public static void main(String[] args) {
+
+        //Clock 提供对当前日期和时间的访问。我们可以利用它来替代 System.currentTimeMillis() 方法。
+        // 另外，通过 clock.instant() 能够获取一个 instant 实例， 此实例能够方便地转换成老版本中的 java.util.Date 对象。
+        Clock clock = Clock.systemDefaultZone();
+        long millis = clock.millis();
+
+        Instant instant = clock.instant();
+        Date legacyDate = Date.from(instant);
+
+
+        // Timezones
+        System.out.println(ZoneId.getAvailableZoneIds());
+        // prints all available timezone ids
+
+        ZoneId zone1 = ZoneId.of("Europe/Berlin");
+        ZoneId zone2 = ZoneId.of("Brazil/East");
+        System.out.println(zone1.getRules());
+        System.out.println(zone2.getRules());
+
+        // ZoneRules[currentStandardOffset=+01:00]
+        // ZoneRules[currentStandardOffset=-03:00]
+
+
+        // LocalTime
+        LocalTime now1 = LocalTime.now(zone1);
+        LocalTime now2 = LocalTime.now(zone2);
+
+        System.out.println(now1.isBefore(now2));  // false
+
+        long hoursBetween = ChronoUnit.HOURS.between(now1, now2);
+        long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);
+
+        System.out.println(hoursBetween);       // -3
+        System.out.println(minutesBetween);     // -239
+
+        LocalTime late = LocalTime.of(23, 59, 59);
+        System.out.println(late);       // 23:59:59
+
+        DateTimeFormatter germanFormatter =
+                DateTimeFormatter
+                        .ofLocalizedTime(FormatStyle.SHORT)
+                        .withLocale(Locale.GERMAN);
+
+        LocalTime leetTime = LocalTime.parse("13:37", germanFormatter);
+        System.out.println(leetTime);   // 13:37
+
+
+        // LocalDate
+        LocalDate today = LocalDate.now();
+        // 今天加一天
+        LocalDate tomorrow = today.plus(1, ChronoUnit.DAYS);
+        // 明天减两天
+        LocalDate yesterday = tomorrow.minusDays(2);
+
+        // 2014 年七月的第四天
+        LocalDate independenceDay = LocalDate.of(2014, Month.JULY, 4);
+        DayOfWeek dayOfWeek = independenceDay.getDayOfWeek();
+        System.out.println(dayOfWeek);    // 星期五
+
+
+        DateTimeFormatter germanFormatter1 =
+                DateTimeFormatter
+                        .ofLocalizedDate(FormatStyle.MEDIUM)
+                        .withLocale(Locale.GERMAN);
+
+        LocalDate xmas = LocalDate.parse("24.12.2014", germanFormatter1);
+        System.out.println(xmas);   // 2014-12-24
+
+
+        // LocalDateTime
+        LocalDateTime sylvester = LocalDateTime.of(2014, Month.DECEMBER, 31, 23, 59, 59);
+
+        DayOfWeek dayOfWeek1 = sylvester.getDayOfWeek();
+        System.out.println(dayOfWeek1);      // 星期三
+
+        Month month = sylvester.getMonth();
+        System.out.println(month);          // 十二月
+
+        // 获取改时间是该天中的第几分钟
+        long minuteOfDay = sylvester.getLong(ChronoField.MINUTE_OF_DAY);
+        System.out.println(minuteOfDay);    // 1439
+
+
+        Instant instant1 = sylvester
+                .atZone(ZoneId.systemDefault())
+                .toInstant();
+
+        Date legacyDate1 = Date.from(instant);
+        System.out.println(legacyDate);     // Wed Dec 31 23:59:59 CET 2014
+
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter
+                        .ofPattern("MMM dd, yyyy - HH:mm");
+
+        LocalDateTime parsed = LocalDateTime.parse("Nov 03, 2014 - 07:13", formatter);
+        String string = formatter.format(parsed);
+        System.out.println(string);     // Nov 03, 2014 - 07:13
+    }
+}
