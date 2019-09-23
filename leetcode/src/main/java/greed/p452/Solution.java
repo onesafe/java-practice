10b130cb7422c59611a51ff9baa83e0dc636e66c
Wang Yiping <wangyiping@4paradigm.com>
2019-09-23 21:04:28 +0800

    LeetCode 452. 用最少数量的箭引爆气球

---
+++ leetcode/src/main/java/greed/p452/Solution.java
@@ -0,0 +1,30 @@
+package greed.p452;
+
+import java.util.Arrays;
+
+/**
+ * LeetCode 452. 用最少数量的箭引爆气球
+ */
+public class Solution {
+
+    public int findMinArrowShots(int[][] points) {
+        if(points.length == 0) {
+            return 0;
+        }
+
+        // 按照右端点排序
+        Arrays.sort(points, (a, b) -> (a[1] - b[1]));
+
+        int shoot_num = 1;
+        int axis = points[0][1];
+
+        for (int i=1; i<points.length; i++) {
+            if (axis < points[i][0]) {
+                shoot_num++;
+                axis = points[i][1];
+            }
+        }
+
+        return shoot_num;
+    }
+}
